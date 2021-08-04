85761277ee192bc0e37457600298919e0424673d
Juanpa1312 <63626319+Juanpa1312@users.noreply.github.com>
2021-08-04 01:29:10 -0600

    Add files via upload

---
+++ prbs_verification.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+Clk = 0
+Reset = 0
+shift_reg = np.array([1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0])
+Semilla = np.array([1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0])
+Longitud = 1
+Polinomio = 1
+reg_1 = 5
+
+
+for i in range(30):
+    if Reset == 0:
+        if i == 0:
+            shift_reg[0:29] = Semilla[0:29]
+            print(shift_reg)
+        else:
+            shift_reg[0:29] = shift_reg << 1
+            for j in range(30):
+                if reg_1 == 5:
+                    reg_1 = shift_reg[j]
+                else:
+                    reg_1 = reg_2
+                reg_2 = shift_reg[j+1]
+                shift_reg[j+1] = reg_1
+            reg_1 = 5
+            if Longitud == 1:
+                Salida = shift_reg[29]
+                shift_reg[0] = (shift_reg[29] ^ shift_reg[19])
+            elif Longitud == 2:
+                Salida = shift_reg[24]
+                shift_reg[0] = (shift_reg[24] ^ shift_reg[14])
+            else:
+                Salida = shift_reg[5]
+                shift_reg[0] = (shift_reg[5] ^ shift_reg[3])
+            print(shift_reg)
